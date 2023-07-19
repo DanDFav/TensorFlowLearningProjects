fc3202f187fa0ac819b74d802766fba2bb067a3c
Dan <dandfav@gmail.com>
2023-07-19 12:35:35 +1000

    Loading handwriting datasets from google

--- js_handwritingData.js
+++ js_handwritingData.js
@@ -1,6 +1,8 @@
 const numberOfDataSetSamples = 65000; 
 const imageSize = 784;
 const chunkSize = 5000; 
+const imagePath = "https://storage.googleapis.com/learnjs-data/model-builder/mnist_images.png";
+const labelPath = "https://storage.googleapis.com/learnjs-data/model-builder/mnist_labels_uint8";
 
 class data {
     async load(){
@@ -9,17 +11,13 @@ class data {
 
         const context = canvas.getContext("2d");
 
-        const imgRequest = new Promise ((resolve, reject) => {
+        const imageRequest = new Promise ((resolve, reject) => {
             image.crossOrigin = ""; 
             image.onload = () => {
-
                 bytesBuffer  = new ArrayBuffer(numberOfDataSetSamples * imageSize * 4); 
-
                 for(let i = 0; i < numberOfDataSetSamples; i++){
                     const imageBytes = new Float32Array(bytesBuffer, i * imageSize * chunkSize * 4, imageSize * chunkSize); 
-
                     context.drawImage(image, 0, i * chunkSize, image.width, chunkSize, 0, 0, image.width, chunkSize)
-
                     const imageData = context.getImageData(0, 0, canvas.width, canvas.height); 
 
                     for(let imageDataIndex = 0; imageDataIndex < imageData.data.length / 4; imageDataIndex++){
@@ -27,11 +25,15 @@ class data {
                     }
 
                 }
-
                 this.datasetImages = new Float32Array(bytesBuffer); 
-
                 resolve(); 
             }
+            image.src = imagePath; 
         });
+
+        const labelsRequest = fetch(labelPath);
+        const [imageResponse, labelsResponse] = await Promise.all([imageRequest, labelsRequest]);
+
+        this.labels = new Uint8Array(await labelsResponse.arrayBuffer());
     }
 }
