5a1f2f555a35acddb3fdf9c014e694b56d9d7800
Dan <dandfav@gmail.com>
2023-07-20 09:29:39 +1000

    Browser and model can recognise directions from voice

--- js_speechRecog.js
+++ js_speechRecog.js
@@ -5,5 +5,30 @@ async function speechRecogMain(){
 
     await modelSR.ensureModelLoaded();
 
-    console.log(modelSR); 
+    console.log("Model Loaded"); 
+
+    const canvas = document.getElementById("drawingCanvas");
+    const context = canvas.getContext("2d");
+
+    context.lineWidth = 5; 
+
+    var xPos = 250; 
+    var yPos = 250; 
+
+    draw(context, xPos, yPos);
+}
+
+function draw(context, xPos, yPos) {
+    const detectedWords = modelSR.wordLabels();
+    
+    modelSR.listen(({scores}) => {
+        scores = Array.from(scores).map((s, index) => ({
+            score: s, word: detectedWords[index]
+        }));
+
+        scores.sort((s1, s2) => s2.score - s1.score);
+
+        var directionOfDrawing = scores[0].word;
+        console.log(directionOfDrawing);
+    });
 }
