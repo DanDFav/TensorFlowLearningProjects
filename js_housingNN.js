059d00719620b3a1d5360028ec2a5feae08dfa2c
Dan <dandfav@gmail.com>
2023-07-20 14:52:01 +1000

    Training the model and visualising the loss and mse on each epoch

--- js_housingNN.js
+++ js_housingNN.js
@@ -1,9 +1,85 @@
 let tensorHousingData; 
+let trainedHousingModel; 
+
+
+async function runHousingModel(){
+    const {inputs, labels} = tensorHousingData; 
+
+    trainedHousingModel = buildHousingModel(); 
+
+    await trainHousingModel(inputs, labels); 
+
+    tfvis.show.modelSummary({
+        name: "Training Sumamary"
+    }, trainedHousingModel);
+}   
+
+
+async function trainHousingModel(x,y){
+    trainedHousingModel.compile({
+        optimizer: tf.train.adam(),
+        loss: tf.losses.meanSquaredError, 
+        metrics: ["mse", "acc"]
+    });
+
+    const batchSize = 1; 
+    const epochs = 20; 
+
+    return await trainedHousingModel.fit(x,y,{
+        batchSize, 
+        epochs, 
+        shuffle: true, 
+        callbacks: tfvis.show.fitCallbacks(
+            { name: "Model Training Results" }, 
+            ["loss", "mse", "acc"], {
+                height: 300, 
+                callback: ["onEpochEnd"]
+            }
+        )
+    });
+
+
+}
+
+function buildHousingModel(){
+    const housingModel = tf.sequential(); 
+
+    housingModel.add(tf.layers.dense({
+        inputShape: [1], 
+        units: 1, 
+        useBias: true
+    }))
+
+    housingModel.add(tf.layers.dense({
+        units: 1, 
+        useBias: true
+    }));
+
+    return housingModel; 
+
+
+}
+
 
 async function loadData(){
     tensorHousingData = convertToTensor(json_housingData);  
+    visualiseData(); 
 
+}
+
+async function visualiseData(){ 
+
+    const data = json_housingData.map(entry => ({
+        x: entry.size, 
+        y: entry.price
+    }));
     
+    tfvis.render.scatterplot(
+        {name: "Price of House vs Size"}, 
+        {values: data }, 
+        {xLabel: "Size", 
+         yLabel: "Price"}
+    ); 
 }
 
 
