e225f0af558c60abf99dfea0bf097ff62676aa86
Dan <dandfav@gmail.com>
2023-07-20 10:14:37 +1000

    Can change line colours now

--- js_speechRecog.js
+++ js_speechRecog.js
@@ -1,28 +1,28 @@
 let modelSR; 
 
+const possibleDirections = ["right", "left", "up", "down"];
+const possibleNumbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
 async function speechRecogMain(){
-    modelSR = speechCommands.create("BROWSER_FFT", "directional4w"); 
+    modelSR = speechCommands.create("BROWSER_FFT"); 
 
     await modelSR.ensureModelLoaded();
 
     console.log("Model Loaded"); 
+    console.log(modelSR); 
 
     const canvas = document.getElementById("drawingCanvas");
     const context = canvas.getContext("2d");
 
     context.lineWidth = 15; 
     context.lineHeight = 15; 
+    context.strokeStyle = "black";
 
     var xPos = 250; 
-    var yPos = 250; 
+    var yPos = 250;
 
-    draw(context, xPos, yPos);
-}
-
-
-function draw(context, xPos, yPos) {
     const detectedWords = modelSR.wordLabels();
-    
+
     modelSR.listen(({scores}) => {
         scores = Array.from(scores).map((s, index) => ({
             score: s, word: detectedWords[index]
@@ -30,19 +30,39 @@ function draw(context, xPos, yPos) {
 
         scores.sort((s1, s2) => s2.score - s1.score);
 
-        var directionOfDrawing = scores[0].word;
-        console.log(directionOfDrawing);
-
-        var [newXPos, newYPos] = findNewPos(xPos, yPos, directionOfDrawing);
-
-        context.moveTo(xPos, yPos);
-        context.lineTo(newXPos, newYPos);
-        context.closePath();
-        context.stroke(); 
-
-        xPos = newXPos; 
-        yPos = newYPos;
+        var detectedWord = scores[0].word;
+        console.log(detectedWord);
+        if(possibleDirections.includes(detectedWord)){
+            [xPos, yPos] = draw(context, xPos, yPos, detectedWord);
+        } else if(possibleNumbers.includes(detectedWord)){
+            context.strokeStyle = newColour(detectedWord); 
+        }
     }, {probabilityThreshold: 0.95});
+}
+
+
+function draw(context, xPos, yPos, directionOfDrawing) {
+    var [newXPos, newYPos] = findNewPos(xPos, yPos, directionOfDrawing);
+
+    context.beginPath();
+    context.moveTo(xPos, yPos);
+    context.lineTo(newXPos, newYPos);
+    context.closePath();
+    context.stroke(); 
+
+    xPos = newXPos; 
+    yPos = newYPos;    
+
+    return [xPos, yPos]
+}
+
+function newColour(colour){
+    return {
+        "zero": "black",
+        "one": "red",
+        "two": "blue",
+        "three": "green",
+    }[colour]
 }
 
 function findNewPos(prevX, prevY, direction){
